ea220e54faf771aa62711ff03db7327e5f16fc8c
‘JENNA <JCLAYTO2@GMAIL.COM>
2022-03-22 19:24:47 -0700

    adding email input

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
-// import functions
+console.log('Welcome!');
 
+let dog = 'Redd';
+
+console.log('My dog name is ${dog}');
+console.log('${dog} is very cute.');
+
+const label = document.getElementById('Label');
+label.style.color = 'brown';
+
+const button = document.getElementById{'button'};
+button.addEventListenerbutton.addEventListener('click', ()=>{})
+// 1. store the element with id email-inputs
+const inputs = document.getElementById('Inputs');
+// 2. add class 'hide' to the element
+// with an id of email-inputs
+// remove the class "hide" from the
+inputs.classList.toggle('hide')
+//3. store the element with id thank-you
 // grab DOM elements
+const Thankyou = document.getElementById('Thankyou');
 
-// set event listeners 
-    // get user input
-    // use user input to update state 
-    // update DOM to reflect the new state
+Thankyou.classList.toggle('hide')
+4. // set event listeners
+// get user input
+// use user input to update state 
+// update DOM to reflect the new state
